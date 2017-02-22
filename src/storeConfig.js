9b8b5cb918ea8b293be4acb218d5f0582d2e58ca
Shelest Denis <dshelest@sdvor.com>
2017-02-22 15:46:57 +0500

    Store config

--- src/storeConfig.js
+++ src/storeConfig.js
@@ -2,14 +2,18 @@ import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './reducers/root';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
+import persistState from 'redux-localstorage';
 
 export default function configureStore(initialState) {
 
-  const store = createStore(rootReducer, initialState,
+  const store = createStore(
+    rootReducer, 
+    initialState,
     composeWithDevTools(
       applyMiddleware(
         thunk
       ),
+      persistState(),
     )
   );
 
